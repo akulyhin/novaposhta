ac95da2ad35774e068006b12ee5665117235cf5e
Artem Kulyhin <akulyhin@gmail.com>
2020-10-18 15:24:38 +0300

    Add search street input

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import './js/searchQuery.js';
 
 
 const search = document.querySelector('#tags');
-
-function resultDetect () {
-    const result = document.querySelector('#ui-id-1');
-}
-setTimeout(resultDetect, 100);
+const street = document.querySelector('#tagsStreet');
 
 let arr = [];
+let arrStreet = [];
 
 search.addEventListener('input', debounce((e) => {
     // console.log(e.target.value);
@@ -54,12 +51,67 @@ search.addEventListener('input', debounce((e) => {
         
         
         function available (availableTags, arr) {
+          const ref = arr.map(city => city.Ref);
             const select = arr.map(city => city.Present);
             availableTags = select;
+            console.log(ref);
             
             return availableTags;
         }
         },
       });
+
+}, 300));
+
+console.log(search.value);
+
+street.addEventListener('input', debounce((e) => {
+  // console.log(e.target.value);
+  $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      url: 'https://api.novaposhta.ua/v2.0/json/',
+      data: JSON.stringify({
+        modelName: 'Address',
+        calledMethod: 'searchSettlementStreets',
+        methodProperties: {
+          StreetName: e.target.value,
+          SettlementRef: 'e718a680-4b33-11e4-ab6d-005056801329',
+          Limit: 5
+        },
+        apiKey: 'c1be1e4c0916d85cce43b88f2a0a9fd1'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      xhrFields: {
+        withCredentials: false 
+      },
+      success: function(texts) {
+          
+        console.log(texts);
+        arr = texts.data[0].Addresses;
+        
+        // console.log(arr);
+
+        $( function() {
+          let availableTags = [];
+          let result = available(availableTags, arr);
+          console.log(result)
+      
+          $( "#tagsStreet" ).autocomplete({
+            source: result
+          });
+      });
+      
+      
+      function available (availableTags, arr) {
+          const select = arr.map(city => city.Present);
+          availableTags = select;
+          
+          return availableTags;
+      }
+      },
+    });
 
 }, 300))
