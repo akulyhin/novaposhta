4ffca77abd39d75c8667757ab1fde31415a9b234
Artem Kulyhin <akulyhin@gmail.com>
2022-02-03 15:20:13 +0200

    Last updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './sass/styles.scss';
 import axios from 'axios';
 
+const testHost = 'http://localhost:3031';
+const workHost = 'https://ak-np.herokuapp.com';
+
 const cityForm = document.getElementById('city');
 
 cityForm.addEventListener('input', (e) => {
 
     if (e.target.value.length > 2) {
-        axios.post('https://ak-np.herokuapp.com/api/novaposhta/getCities', {
+        axios.post(`${testHost}/api/novaposhta/getCities`, {
             "query": e.target.value
         })
         .then(res => console.log(res.data));
