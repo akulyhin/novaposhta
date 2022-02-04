56dbda76c78f67d93ede0f0f3003ab15b7eade80
Artem Kulyhin <akulyhin@gmail.com>
2022-02-04 15:11:17 +0200

    Last updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './sass/styles.scss';
 import axios from 'axios';
+import debounce from 'lodash.debounce';
+
 
 const testHost = 'http://localhost:3031';
 const workHost = 'https://ak-np.herokuapp.com';
@@ -9,7 +11,27 @@ const city_autocomplete = document.getElementById('city_autocomplete');
 const warehouseForm = document.getElementById('warehouse');
 const warehouse_autocomplete = document.getElementById('warehouse_autocomplete');
 
-cityForm.addEventListener('input', (e) => {
+let cityRef = '';
+
+warehouse_autocomplete.addEventListener('click', (e) => {
+    warehouseForm.value = e.target.innerText;
+    warehouse_autocomplete.innerHTML = '';
+    warehouse_autocomplete.classList.remove('active');
+});
+
+city_autocomplete.addEventListener('click', (e) => {
+    cityForm.value = e.target.innerText;
+    cityRef = e.target.getAttribute('data-id');
+    city_autocomplete.innerHTML = '';
+    city_autocomplete.classList.remove('active');
+});
+
+cityForm.addEventListener('input', debounce((e) => {
+
+    if (!e.target.value.length) {
+        city_autocomplete.innerHTML = '';
+        city_autocomplete.classList.remove('active');
+    }
 
     if (e.target.value.length > 2) {
         axios.post(`${testHost}/api/novaposhta/getCities`, {
@@ -17,14 +39,27 @@ cityForm.addEventListener('input', (e) => {
         })
         .then(res => {
             city_autocomplete.innerHTML = '';
+            city_autocomplete.classList.remove('active');
+            city_autocomplete.classList.add('active');
             res.data.data.forEach(item => {
-                city_autocomplete.insertAdjacentHTML('beforeend', `<li>${item.DescriptionRu}</li>`) 
+                city_autocomplete.insertAdjacentHTML('beforeend', `<li data-id="${item.Ref}">${item.DescriptionRu}</li>`) 
             })
-            console.log(res.data.data)
         });
-    }
-})
 
-warehouseForm.addEventListener('input', (e) => {
-    
-})
+    }
+}, 300))
+
+warehouseForm.addEventListener('input', debounce((e) => {
+    axios.post(`${testHost}/api/novaposhta/getWarehouses`, {
+        "Ref": cityRef,
+        "query": e.target.value
+    })
+    .then(res => {
+        console.log(res.data.data);
+        warehouse_autocomplete.innerHTML = '';
+        warehouse_autocomplete.classList.add('active');
+        res.data.data.forEach(item => {
+            warehouse_autocomplete.insertAdjacentHTML('beforeend', `<li>${item.DescriptionRu}</li>`);
+        })
+    })
+}, 300))
